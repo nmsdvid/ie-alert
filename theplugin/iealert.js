30e3f482ad8e94ff19ec035cc2ce6c549a3ab6d8
Yijie.Z <yijo1001@gmail.com>
2014-02-20 23:39:01 +0800

    fire iealerted event if message alerted

--- theplugin/iealert.js
+++ theplugin/iealert.js
@@ -7,8 +7,8 @@
 
 (function ($) {
     function initialize($obj, support, title, text, upgradeTitle, upgradeLink, overlayClose, closeBtn) {
-    
-    
+
+
         var panel = "<div class='ie-l-t-c'></div>"
             + "<div class='ie-t'></div>"
             + "<div class='ie-r-t-c'></div>"
@@ -75,6 +75,8 @@
                 overlay.css("background", "#d6d6d6");
                 $obj.css("margin", "0");
             }
+
+            $obj.trigger('iealerted', ie); // fire if message alerted
         }
 
         if (support === "ie9") {            // the modal box will appear on IE9, IE8, IE7, IE6
@@ -113,21 +115,21 @@
         var option = $.extend(defaults, options);
 
         return this.each(function () {
-        	
+
 	    	ie = (function(){
-	 
+
 			    var undef,
 			        v = 3,
 			        div = document.createElement('div'),
 			        all = div.getElementsByTagName('i');
-			    
+
 			    while (
 			        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
 			        all[0]
 			    );
-			    
+
 			    return v > 4 ? v : undef;
-	    
+
 	    	 }());
 
 	    	 // If browser is Internet Explorer
